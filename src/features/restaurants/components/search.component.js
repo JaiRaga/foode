94480a99b27875296960f1c7a75e1741253d0b40
JaiRaga <raga.j.santhosh@gmail.com>
2021-11-22 21:00:06 +0530

    :sparkles: connected to location context to pass the search keyword to search function in location context

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import styled from 'styled-components/native'
 
 import { Searchbar } from 'react-native-paper'
+import { LocationContext } from '../../../services/location/location.context'
 
 const SearchContainer = styled.View`
 	padding: ${(props) => props.theme.space[2]};
@@ -9,9 +10,26 @@ const SearchContainer = styled.View`
 `
 
 export const Search = () => {
+	const { keyword, search } = useContext(LocationContext)
+	const [searchKeyword, setSearchKeyword] = useState(keyword)
+
+	useEffect(() => {
+		search(searchKeyword)
+		console.log('search screen search keyword', searchKeyword)
+	}, [])
+
 	return (
 		<SearchContainer>
-			<Searchbar placeholder='Search' />
+			<Searchbar
+				placeholder='Search for a location'
+				value={searchKeyword}
+				onSubmitEditing={() => {
+					search(searchKeyword)
+				}}
+				onChangeText={(text) => {
+					setSearchKeyword(text)
+				}}
+			/>
 		</SearchContainer>
 	)
 }
