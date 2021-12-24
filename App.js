edb3c6a8a8095daf1397b28e1b7229c7cdb00154
JaiRaga <raga.j.santhosh@gmail.com>
2021-12-25 04:05:30 +0530

    :sparkles: refactored navigation

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar'
-import { Text } from 'react-native'
 import { ThemeProvider } from 'styled-components/native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { Ionicons } from '@expo/vector-icons'
 
 import {
 	useFonts as useOswald,
@@ -13,41 +9,10 @@ import {
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato'
 
 import { theme } from './src/infrastructure/theme'
-import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen'
-import { SafeArea } from './src/components/utility/safe-area.component'
+
 import { RestaurantContextProvider } from './src/services/restaurants/restaurants.context'
 import { LocationContextProvider } from './src/services/location/location.context'
-
-const Tab = createBottomTabNavigator()
-
-// Icons for each tab
-const TAB_ICON = {
-	Restaurants: 'md-restaurant',
-	Map: 'md-map',
-	Settings: 'md-settings',
-}
-
-const Settings = () => (
-	<SafeArea>
-		<Text>Settings</Text>
-	</SafeArea>
-)
-
-const Map = () => (
-	<SafeArea>
-		<Text>Map</Text>
-	</SafeArea>
-)
-
-// displays tabs for screen change
-const createScreenOptions = ({ route }) => {
-	const iconName = TAB_ICON[route.name]
-	return {
-		tabBarIcon: ({ size, color }) => (
-			<Ionicons name={iconName} size={size} color={color} />
-		),
-	}
-}
+import { Navigation } from './src/infrastructure/navigation'
 
 export default function App() {
 	const [oswaldLoaded] = useOswald({
@@ -67,25 +32,7 @@ export default function App() {
 			<ThemeProvider theme={theme}>
 				<LocationContextProvider>
 					<RestaurantContextProvider>
-						<NavigationContainer>
-							<Tab.Navigator screenOptions={createScreenOptions}>
-								<Tab.Screen
-									name='Restaurants'
-									component={RestaurantsScreen}
-									options={{ headerShown: false }}
-								/>
-								<Tab.Screen
-									name='Map'
-									component={Map}
-									options={{ headerShown: false }}
-								/>
-								<Tab.Screen
-									name='Settings'
-									component={Settings}
-									options={{ headerShown: false }}
-								/>
-							</Tab.Navigator>
-						</NavigationContainer>
+						<Navigation />
 					</RestaurantContextProvider>
 				</LocationContextProvider>
 			</ThemeProvider>
