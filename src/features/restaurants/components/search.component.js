9eaf2abde805f497282a3fb69a4d831b859816b7
JaiRaga <raga.j.santhosh@gmail.com>
2022-01-10 21:42:18 +0530

    :hammer: refactored code

--- src/features/restaurants/components/search.component.js
+++ src/features/restaurants/components/search.component.js
@@ -13,11 +13,6 @@ export const Search = () => {
 	const { keyword, search } = useContext(LocationContext)
 	const [searchKeyword, setSearchKeyword] = useState(keyword)
 
-	useEffect(() => {
-		search(searchKeyword)
-		console.log('search screen search keyword', searchKeyword)
-	}, [])
-
 	return (
 		<SearchContainer>
 			<Searchbar
